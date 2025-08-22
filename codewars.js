9f2dabdc1bc586cf50413b92851478a4f1dfc79d
rayne <pothong.r@gmail.com>
2025-08-21 21:42:18 -0700

    Changes to be committed: modified:   codewars.js

--- codewars.js
+++ codewars.js
@@ -224,3 +224,8 @@ function findNeedle(haystack) {
   function removeExclamationMarks(s) {
     return s.replaceAll('!', '')
   }
+  
+// 29 - 8 kyu - Grasshopper - Personalized Message
+  function greet (name, owner) {
+    return name === owner ? 'Hello boss' : 'Hello guest'
+  }
